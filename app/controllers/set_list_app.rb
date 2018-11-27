5d283077ab53cd0a4e53f77bfcd5cf95d00f4b3c
Dione Wilson <dionew1@gmail.com>
2018-11-27 09:29:59 -0700

    Add song show path and view

--- app/controllers/set_list_app.rb
+++ app/controllers/set_list_app.rb
@@ -1,6 +1,11 @@
 class SetList < Sinatra::Base
-  get '/songs' do 
+  get '/songs' do
     @songs = Song.all
     erb :"songs/index"
+  end
+
+  get '/songs/:id' do
+    @song = Song.find(params[:id])
+    erb :"songs/show"
   end
 end
