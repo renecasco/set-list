22fbb2b2fa1e516e8569d9695fa3b7df7d5676c1
ian douglas <ian.douglas@iandouglas.com>
2019-01-23 15:52:44 -0700

    passing data in sinatra, working form and database update

--- app/controllers/set_list_app.rb
+++ app/controllers/set_list_app.rb
@@ -3,4 +3,20 @@ class SetList < Sinatra::Base
     @songs = Song.all
     erb :"songs/index"
   end
+
+  get '/songs/:id' do
+    @song = Song.find(params[:id])
+    erb :"songs/show"
+  end
+
+  get '/songs/:id/edit' do
+    @song = Song.find(params[:id])
+    erb :"songs/edit"
+  end
+
+  put '/songs/:id' do
+    @song = Song.find(params[:id])
+    @song.update(params[:song])
+    redirect '/songs'
+  end
 end
